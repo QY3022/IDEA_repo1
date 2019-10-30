fa50ec5bdbb38346653d52658007ce02e689ece7
丘健里 <970222897@qq.com>
2019-10-30 10:51:03 +0800

    第一次将IDEA里的代码添加到本地版本库

---
+++ 1/src/com/java/Hello.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+        hello();
+    }
+
+    public static void hello(){
+        System.out.println("I love you");
+    }
+}
